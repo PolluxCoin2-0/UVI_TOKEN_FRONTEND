f112cf3d551e2811b229434657ec2c23f4471f90
DurgeshY21 <durgeshy5226@gmail.com>
2024-08-29 16:09:23 +0530

    alignment

--- src/pages/auth/VerifyReferral.jsx
+++ src/pages/auth/VerifyReferral.jsx
@@ -34,25 +34,25 @@ const VerifyReferral = () => {
         alt="background"
         className="absolute inset-0 w-full h-full object-cover opacity-30"
       />
-      <div className="bg-black rounded-2xl shadow-custom shadow-gray-800 w-full max-w-md md:max-w-lg lg:max-w-xl p-8 relative z-10 mt-16 md:mt-0 mx-4 md:mx-0 flex flex-col items-center ">
+      <div className="bg-black rounded-2xl shadow-custom shadow-gray-800 w-full max-w-md md:max-w-lg lg:max-w-xl p-0 relative z-10 mt-16 md:mt-0 mx-4 md:mx-0 flex flex-col items-center  ">
         {/* connect wallet image */}
         <div>
           <img src={LogoImg} className="" />
         </div>
 
-        <div className="flex flex-col space-y-2">
+        <div className="flex flex-col space-y-2 ">
           <input
             id="text-input"
             type="text"
             value={value}
             onChange={handleChange}
-            className="w-96 px-4 py-3 border  border-gray-300 rounded-lg shadow-sm focus:outline-none  transition duration-150 ease-in-out"
+            className="w-80 md:w-96 px-4 py-3 border  border-gray-300 rounded-lg shadow-sm focus:outline-none  transition duration-150 ease-in-out  "
             placeholder="Enter Wallet Address"
           />
         </div>
         <button
           type="button"
-          className="bg-white px-10 md:px-20 py-2 md:py-3 rounded-lg text-black text-lg font-bold mt-10 hover:bg-[#F3BB1C]"
+          className="bg-white px-8 md:px-20 py-2 md:py-3 rounded-2xl text-black text-lg font-bold mt-10 hover:bg-[#F3BB1C] mb-8"
           onClick={verifyReferralfunc}
         >
           Verify
