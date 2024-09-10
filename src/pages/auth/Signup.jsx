d4062770c4a0d1bf4ab82eb764a33c65645d227f
gshubham804 <gshubham804@gmail.com>
2024-09-10 22:27:32 +0530

    code pushed

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
   const handleSubmit = async(e) => {
     e.preventDefault();
 
-    if(!email && !walletAddress){
+    if(!email && walletAddress.length>0){
       toast.error("Please enter your email and wallet address!");
       return;
     }
