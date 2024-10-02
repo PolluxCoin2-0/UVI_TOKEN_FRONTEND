032a67e973091c826c2db170aa22152e6da1a17c
gshubham804 <gshubham804@gmail.com>
2024-10-02 15:22:39 +0530

    changed the timer

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,10 +4,10 @@ import { setCurrentSlotNumber } from "../redux/slice/SlotsSlice";
 
 // Define the time slots with labels, values, and colors
 const intervals = [
-  { label: "Slot No: 1/4 (15:45 - 21:45)", value: 6, color: "#FFA21B", bgColor: "rgba(255, 162, 27, 0.3)" },
-  { label: "Slot No: 2/4 (21:45 - 03:45)", value: 12, color: "#6B8BFC", bgColor: "rgba(107, 139, 252, 0.3)" },
-  { label: "Slot No: 3/4 (03:45 - 09:45)", value: 18, color: "#FFCC07", bgColor: "rgba(255, 204, 7, 0.3)" },
-  { label: "Slot No: 4/4 (09:45 - 15:45)", value: 24, color: "#0098FE", bgColor: "rgba(0, 152, 254, 0.3)" },
+  { label: "Slot No: 1/4 (19:45 - 01:45)", value: 6, color: "#FFA21B", bgColor: "rgba(255, 162, 27, 0.3)" },
+  { label: "Slot No: 2/4 (01:45 - 07:45)", value: 12, color: "#6B8BFC", bgColor: "rgba(107, 139, 252, 0.3)" },
+  { label: "Slot No: 3/4 (07:45 - 13:45)", value: 18, color: "#FFCC07", bgColor: "rgba(255, 204, 7, 0.3)" },
+  { label: "Slot No: 4/4 (13:45 - 19:45)", value: 24, color: "#0098FE", bgColor: "rgba(0, 152, 254, 0.3)" },
 ];
 
 // Helper function to calculate percentage progress
